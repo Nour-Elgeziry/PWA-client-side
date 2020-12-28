4a418e84ac587a98d4c443579cd3673a76bff409
Nour Elgeziry <elgezirn@coventry.ac.uk>
2020-12-28 07:18:55 +0000

    Connected koa router to project index.html and static files

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 
 /* index.js */
 
-import Koa from 'koa'
+
 import https from 'https'
+import Koa from 'koa'
+import Router from 'koa-router'
+import serve from 'koa-static'
+import views from 'koa-views'
+import fs from 'fs-extra'
+
 
 const app = new Koa()
+const router = new Router()
 
 const defaultPort = 8080
 const port = process.env.PORT || defaultPort
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
+
+
+app.use(serve('.'))
+
+app.use(views('.'))
+
+
+router.get('/', async ctx => {
+	
+	await ctx.render('index.html')
 })
+
+app.use(router.routes())
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
+
